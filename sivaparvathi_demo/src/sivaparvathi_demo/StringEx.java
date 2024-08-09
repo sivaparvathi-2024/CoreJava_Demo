d6c615ae72077ac4a62383be9770a4d7f391f9b8
sivaparvathijava2016@gmail.com <ssiva@SIVAPARVATHI>
2024-08-09 13:04:17 +0530

    The project is added

---
+++ sivaparvathi_demo/src/sivaparvathi_demo/StringEx.java
@@ -0,0 +1,16 @@
+package sivaparvathi_demo;
+
+public class StringEx {
+	public static void main(String[] args) {
+
+		String str = "xyz";
+		String str1 = "abc";
+		String str2 = "xyz";
+		
+		System.out.println(str.equals(str2));	
+		
+		System.out.println(str.equalsIgnoreCase(str1));
+		
+	}
+
+}
